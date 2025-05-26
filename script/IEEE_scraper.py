a76c3fd9e03c6a9ab3daedd4187ff9d3628f0b9b
Davide Torielli <toridebraus@gmail.com>
2025-05-26 10:27:03 -0400

    up

--- script/IEEE_scraper.py
+++ script/IEEE_scraper.py
@@ -123,8 +123,8 @@ def main():
 
         if not (scraped_conf.shortName.upper() in existent_confs_dict[scraped_conf.year]):
             #ignore roman already present because of bad format
-            if (scraped_conf.name == "34th IEEE International Conference on Robot and Human Interactive Communication (RO-MAN"):
-                continue
+            # if (scraped_conf.name == "34th IEEE International Conference on Robot and Human Interactive Communication (RO-MAN)"):
+            #     continue
             print("Adding ", scraped_conf.shortName, scraped_conf.year)
             json_file_to_update[scraped_conf.year] = True
 
